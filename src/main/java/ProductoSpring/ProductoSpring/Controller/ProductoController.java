6dc68fd4485939bff819c31e8dc36060077b7a23
Aklan32 <mateo2811123@gmail.com>
2025-05-27 17:37:35 -0500

    documentacion de API con Swogger

--- src/main/java/ProductoSpring/ProductoSpring/Controller/ProductoController.java
+++ src/main/java/ProductoSpring/ProductoSpring/Controller/ProductoController.java
@@ -1,6 +1,8 @@
 package ProductoSpring.ProductoSpring.Controller;
 import ProductoSpring.ProductoSpring.Modelo.Producto;
 import ProductoSpring.ProductoSpring.Repository.ProductoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +18,53 @@ import java.util.List;
 import java.util.HashMap;
 @RestController
 @RequestMapping("/productos")
+@Tag(name="API CONTROLLER")
+
 public class ProductoController {
     @Autowired
     private ProductoRepository productoRepository;
+
+    @Operation (
+            summary = "Listar Productos",
+            description = "devuelve un listado de todos los productos"
+    )
     @GetMapping
     public List<Producto> listarProducto() {
         return productoRepository.listarProducto();
     }
+    @Operation (
+            summary = "Buscar por Id",
+            description = "Nos busca el producto por su ID"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Producto> buscarPorId(@PathVariable int id) {
         Producto producto = productoRepository.buscarPorId(id);
         return ResponseEntity.ok(producto);
     }
+    @Operation (
+            summary = "InsertarProducto",
+            description = "Insertamos dentro de la lista un Producto"
+    )
     @PostMapping
     public ResponseEntity<String> InsertarProducto(@RequestBody Producto producto) {
         productoRepository.InsertarProducto(producto);
         return ResponseEntity.ok("EL producto insertado");
     }
+    @Operation (
+            summary = "EliminaProducto",
+            description = "Elimina un producto de la lista"
+
+    )
     @DeleteMapping
     public ResponseEntity<String> EliminarProducto(@RequestBody Producto producto) {
         productoRepository.eliminarPorNombre(producto.getNombre());
         return ResponseEntity.ok("PRODUCTO ELIMINADO");
     }
+    @Operation (
+            summary = "ActualizaProducto",
+            description = "Modifica un producto de la lista"
+
+    )
     @PutMapping
     public ResponseEntity<Map<String, String>> actualizarProducto(@RequestBody Producto producto) {
         productoRepository.actualizarProducto(producto);
@@ -47,4 +74,5 @@ public class ProductoController {
 
         return ResponseEntity.ok(response);
     }
+
 }
--- src/main/java/ProductoSpring/ProductoSpring/Modelo/Producto.java
+++ src/main/java/ProductoSpring/ProductoSpring/Modelo/Producto.java
@@ -1,11 +1,28 @@
 package ProductoSpring.ProductoSpring.Modelo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 @Data
 public class Producto
 {
+    @Schema (
+            description = "identificador de la tabla"
+    )
     private int  id;
+    @Schema (
+            description = "nombre del producto"
+    )
+
     private String nombre;
+    @Schema (
+            description = "precio del producto"
+    )
     private Double precio;
+    @Schema (
+            description = "cantidad del producto",
+            required = true,
+            example = "23"
+
+    )
     private int stock;
 
 }
